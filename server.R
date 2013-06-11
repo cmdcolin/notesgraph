b7e054c6f9a085f27e47ae62ddc33327dc5903c7
grubble <colin.diesh@gmail.com>
2013-06-11 06:20:06 +0000

    Remove animation code, fix regex

--- server.R
+++ server.R
@@ -18,8 +18,8 @@ shinyServer(function(input, output) {
   output$myoutput <- renderPlot({
     myfile<-strsplit(input$users,'\n')
     # grep 
-    reblogSet<-str_match(myfile[[1]],"(.*) reblogged this from (.*)")
-    whoposted<-str_match(myfile[[1]],"(.*) posted this")
+    reblogSet<-str_match(myfile[[1]],"([^ ]*) reblogged this from ([^ ]*)")
+    whoposted<-str_match(myfile[[1]],"([^ ]*) posted this")
     
     #remove non-reblog lines
     reblogSet<-reblogSet[!is.na(reblogSet[,1]),]
@@ -56,67 +56,6 @@ shinyServer(function(input, output) {
     renderGraph(x,graph.pars=list(overlap=FALSE))
   })
   
-  
-  
-  
-  output$myanimation<-renderUI({
-    ## A quick and dirty demo
-    outdir=tempdir()
-    loutdir=unlist(strsplit(outdir,'/'))
-    myoutdir=paste0('tmp/',loutdir[3])
-    dir.create(paste0('www/',myoutdir))
-    saveHTML({
-      myfile<-strsplit(input$users,'\n')
-      reblogSet<-str_match(myfile[[1]],"(.*) reblogged this from (.*)")
-      whoposted<-str_match(myfile[[1]],"(.*) posted this")
-      
-      #remove non-reblog lines
-      reblogSet<-reblogSet[!is.na(reblogSet[,1]),]
-      whoposted<-whoposted[!is.na(whoposted[,1]),]
-      
-      nr<-nrow(reblogSet)
-      for (i in 2:nrow(reblogSet)) {
-        # grep 
-        
-        allBlogs<-unique(c(reblogSet[(nr-i):nr,2],reblogSet[(nr-i):nr,3]))
-        rEG <- new("graphNEL", nodes=allBlogs, edgemode="directed")
-        apply(reblogSet[(nr-i):nr,],1,function(row) {
-          rEG<<-addEdge(row[3],row[2],rEG,1)
-        })
-        attrs <- list(node=list(shape="ellipse", fixedsize=FALSE,overlap=FALSE))
-        mydistances<-sp.between(rEG,whoposted[2],nodes(rEG),detail=FALSE)
-        
-        mydistances<-as.vector(unlist(mydistances))
-        mydistances[is.na(mydistances)]=0
-        
-        nA=list()
-        nNodes <- length(nodes(rEG))
-        nA$fontsize <- rep(12, nNodes)
-        nA$fillcolor=(mydistances)+1
-        nA <- lapply(nA, function(x) { names(x) <- nodes(rEG); x})
-        #plot(z, nodeAttrs=nA,attrs=attrs)
-        
-        ncol=max(mydistances)+3
-        mypal<-switch(input$palettetype,rainbow=rainbow(ncol),
-                      topo.colors=topo.colors(ncol),
-                      terrain.colors=terrain.colors(ncol),
-                      heat.colors=heat.colors(ncol),
-                      cm.colors=cm.colors(ncol))
-        palette(mypal)
-        x <- layoutGraph(rEG,nodeAttrs=nA, layoutType=input$graphtype,attrs=attrs)
-        
-        renderGraph(x,graph.pars=list(overlap=FALSE))
-        ani.pause()
-      }
-    }, img.name = "anim_plot", imgdir = "img", autobrowse = FALSE, 
-             title = "Demo of animation", 
-             description = c("This is a NoteGraph animation"),
-             htmlfile='animation.html',outdir=paste0('www/',myoutdir))
-    
-    tags$iframe(src=paste0(myoutdir,'/animation.html'), 
-                id="aniframe", 
-                style="width:100%;height:800px;")
-  })
   
   
   
